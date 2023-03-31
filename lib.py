ff5dc16064f2d5817e2cbe8d85c6c2bf89f5cd76
ShibinWu <53476135+AustinNgs@users.noreply.github.com>
2023-03-31 20:02:39 +0800

    Update lib.py

--- lib.py
+++ lib.py
@@ -7,14 +7,12 @@ sys.path[0] = '/home/lab-wu.shibin/SFDA'
 def reverse_sigmoid(y):
     return torch.log(y / (1.0 - y + 1e-10) + 1e-10)
 
-
 def normalize_weight(x):
     min_val = x.min()
     max_val = x.max()
     x = (x - min_val) / (max_val - min_val)
     x = x / torch.mean(x)
     return x.detach()
-
 
 def seed_everything(seed=1234):
     import random
