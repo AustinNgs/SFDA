b1345108c794911722ffc50a0979b4cdfa7d4ac1
ShibinWu <53476135+AustinNgs@users.noreply.github.com>
2023-04-01 12:03:13 +0800

    Update csv2pic.py

--- csv2pic.py
+++ csv2pic.py
@@ -64,31 +64,24 @@ train_transform = Compose([ #Tansform setting: resnet-50-resize224; vggnet-16-re
     CenterCrop(224),
     ToTensor()
 ])
-test_transform = Compose([
+test_transform = Compose([ #Tansform setting: resnet-50-resize224; vggnet-16-resize256-crop224
     Resize(256,interpolation=Image.LANCZOS),
     CenterCrop(224),
     ToTensor()
 ])
 
 source_train_ds = DatasetFromCSV(list_path=source_file, path_prefix=dataset.prefixes[args.data.dataset.source],
-                            transform=train_transform#, filter=(lambda x: x in source_classes)
-                            )
-train_size = int(len(source_train_ds) * 0.8)
-val_size = len(source_train_ds) - train_size
-
-source_train_ds, source_test_ds = torch.utils.data.random_split(source_train_ds, [len(source_train_ds)-600, 600]) #1400
-
+                            transform=train_transform)
+source_test_ds = DatasetFromCSV(list_path=source_file, path_prefix=dataset.prefixes[args.data.dataset.source],
+                            transform=test_transform)
 target_train_ds = DatasetFromCSV(list_path=target_file, path_prefix=dataset.prefixes[args.data.dataset.target],
-                            transform=train_transform#, filter=(lambda x: x in target_classes)
-                            )
+                            transform=train_transform)
 target_test_ds = DatasetFromCSV(list_path=target_file, path_prefix=dataset.prefixes[args.data.dataset.target],
-                            transform=test_transform#, filter=(lambda x: x in target_classes)
-                            )
+                            transform=test_transform)
 source_train_dl = DataLoader(dataset=source_train_ds, batch_size=args.data.dataloader.batch_size,shuffle=True,
-                             #sampler=sampler, 
                              num_workers=args.data.dataloader.data_workers, drop_last=True)
 source_test_dl = DataLoader(dataset=source_test_ds, batch_size=args.data.dataloader.batch_size, shuffle=True,
-                             num_workers=1, drop_last=False)
+                             num_workers=1, drop_last=True)
 target_train_dl = DataLoader(dataset=target_train_ds, batch_size=args.data.dataloader.batch_size,shuffle=True,
                              num_workers=args.data.dataloader.data_workers, drop_last=True)
 target_test_dl = DataLoader(dataset=target_test_ds, batch_size=args.data.dataloader.batch_size, shuffle=True,
