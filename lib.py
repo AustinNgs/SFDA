9007b2a1334f421041811f460c7d3e9cbfded696
ShibinWu <53476135+AustinNgs@users.noreply.github.com>
2023-11-28 14:56:53 +0800

    Update lib.py

--- lib.py
+++ lib.py
@@ -22,3 +22,28 @@ def seed_everything(seed=1234):
     np.random.seed(seed)
     import os
     os.environ['PYTHONHASHSEED'] = str(seed)
+
+def euclidean_dist(x, y=None):
+    """
+    Compute all pairwise distances between vectors in X and Y matrices.
+    :param x: numpy array, with size of (d, m)
+    :param y: numpy array, with size of (d, n)
+    :return: EDM:   numpy array, with size of (m,n). 
+                    Each entry in EDM_{i,j} represents the distance between row i in x and row j in y.
+    """
+    if y is None:
+        y = x
+
+    # calculate Gram matrices
+    G_x = np.matmul(x.T, x)
+    G_y = np.matmul(y.T, y)
+
+    # convert diagonal matrix into column vector
+    diag_Gx = np.reshape(np.diag(G_x), (-1, 1))
+    diag_Gy = np.reshape(np.diag(G_y), (-1, 1))
+
+    # Compute Euclidean distance matrix
+    EDM = diag_Gx + diag_Gy.T - 2*np.matmul(x.T, y)  # broadcasting
+    d = np.sqrt(EDM)
+    return d.sum()
+    
